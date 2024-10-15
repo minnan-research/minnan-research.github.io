06635421e288e3d673e106d4ae2244a12fbc4597
jxlab <app.jxlab@gmail.com>
2024-10-15 15:30:58 +0800

    Initial commit

---
+++ do_pyftsubset.bat
@@ -0,0 +1,3 @@
+type kautian.csv docs\mispronunciation\index.html > combined.txt
+pyftsubset docs/fonts/Iansui-Regular.ttf --text-file=combined.txt --output-file=docs/fonts/Iansui-Regular-subset.ttf
+del combined.txt
