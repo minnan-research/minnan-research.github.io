d8fd053dbff2d59a4c952a9037f8198eb9d75927
jxlab <app.jxlab@gmail.com>
2024-10-17 16:48:47 +0800

    Add more chars for font subset
    Also for mistranslation index.md

--- do_pyftsubset.bat
+++ do_pyftsubset.bat
@@ -1,3 +1,8 @@
-type kautian.csv docs\mispronunciation\index.html > combined.txt
+setlocal
+
+cd /d "%~dp0"
+
+type other_chars.txt kautian.csv docs\mispronunciation\index.html docs\mistranslation\index.md > combined.txt
 pyftsubset docs/fonts/Iansui-Regular.ttf --text-file=combined.txt --output-file=docs/fonts/Iansui-Regular-subset.ttf
 del combined.txt
+pause
