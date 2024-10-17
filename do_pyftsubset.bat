e3b44e4f5b2ba50da6148f7d43e6bd1d96a4c68d
jxlab <app.jxlab@gmail.com>
2024-10-17 18:05:15 +0800

    Update do_pyftsubset.bat

--- do_pyftsubset.bat
+++ do_pyftsubset.bat
@@ -1,7 +1,4 @@
-setlocal
-
 cd /d "%~dp0"
-
 type other_chars.txt kautian.csv docs\mispronunciation\index.html docs\mistranslation\index.md > combined.txt
 pyftsubset docs/fonts/Iansui-Regular.ttf --text-file=combined.txt --output-file=docs/fonts/Iansui-Regular-subset.ttf
 del combined.txt
