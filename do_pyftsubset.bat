fd7eb0e7c0a1210219a6aa38faab53c89da248d4
jxlab <app.jxlab@gmail.com>
2024-10-24 12:05:17 +0800

    Use tauhu-oo font

--- do_pyftsubset.bat
+++ do_pyftsubset.bat
@@ -1,5 +1,7 @@
 cd /d "%~dp0"
 type other_chars.txt kautian.csv docs\mispronunciation\index.html docs\mistranslation\index.md > combined.txt
-pyftsubset docs/fonts/Iansui-Regular.ttf --text-file=combined.txt --output-file=docs/fonts/Iansui-Regular-subset.ttf
+pyftsubset docs/fonts/TauhuOo20.05-Regular.woff2 --text-file=combined.txt --output-file=docs/fonts/TauhuOo20.05-Regular-subset.woff2 --flavor=woff2
+pyftsubset docs/fonts/SourceHanSansTC-Bold.otf --text-file=docs\mistranslation\index.md --output-file=docs/fonts/SourceHanSansTC-Bold-subset.woff2 --flavor=woff2
+
 del combined.txt
 pause
